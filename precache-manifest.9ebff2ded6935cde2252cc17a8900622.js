8f6997a1b2472abaaf442cb0ea4de6306f010ea9
Mateus <mat3usrc@gmail.com>
2018-11-08 14:39:43 -0200

    Updates

---
+++ precache-manifest.9ebff2ded6935cde2252cc17a8900622.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "ac8ec3d4ed91e5e7b560",
+    "url": "/portfolio/static/js/main.ac8ec3d4.chunk.js"
+  },
+  {
+    "revision": "90c69224f39e7c70638f",
+    "url": "/portfolio/static/js/1.90c69224.chunk.js"
+  },
+  {
+    "revision": "ac8ec3d4ed91e5e7b560",
+    "url": "/portfolio/static/css/main.468e4690.chunk.css"
+  },
+  {
+    "revision": "e08c0492ec14b1ec162bc70dabe22f36",
+    "url": "/portfolio/index.html"
+  }
+];
